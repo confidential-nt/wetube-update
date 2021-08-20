8c50a310f0f82149c7caa4d4e6ca4f0f976d21be
confidential-nt <youyoy689@gmail.com>
2021-08-21 06:17:46 +0900

    web assembly 1

---
+++ src/client/js/recorder.js
@@ -0,0 +1,54 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+import { async } from "regenerator-runtime";
+
+const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
+
+let stream;
+let recorder;
+let videoFile;
+
+const handleDownload = async () => {
+  const ffmpeg = createFFmpeg({ log: true }); // log => 콘솔에서 무슨 일이 벌어지고 있는지 확인가능.
+  await ffmpeg.load();
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording.webm";
+  document.body.appendChild(a);
+  a.click();
+};
+
+const handleStop = () => {
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
+  recorder.ondataavailable = (event) => {
+    videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: true,
+  });
+  video.srcObject = stream;
+  video.play();
+};
+
+init();
+
+startBtn.addEventListener("click", handleStart);
